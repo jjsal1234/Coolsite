a9f8aa2c7a5013ab8a8cabf7650ebb2a9a9ab4b8
jjsal1234 <143920177+jjsal1234@users.noreply.github.com>
2023-11-22 02:48:25 -0700

    downgrade.scipt.js

--- script.js
+++ script.js
@@ -37,21 +37,23 @@ function isSignedIn() {
 
 // Function to update the visibility of sign-in and profile buttons
 function updateSignInStatus() {
-    var profileButton = document.getElementById("profileButton");
-    var signInButton = document.getElementById("signInButton");
+    const signInButton = document.getElementById("signInButton");
+    const profileButton = document.getElementById("profileButton");
 
-    if (profileButton && signInButton) {
-        if (userIsSignedIn()) {
-            // User is signed in
-            var username = getSignedInUsername();
-            profileButton.style.display = "block";
-            profileButton.innerHTML = "Welcome, " + username + "!";
-            signInButton.style.display = "none";
-        } else {
-            // User is not signed in
-            profileButton.style.display = "none";
-            signInButton.style.display = "block";
-        }
+    if (isSignedIn()) {
+        signInButton.style.display = "none";
+        profileButton.style.display = "inline-block";
+
+        // Display the current account info
+        const signedInUser = localStorage.getItem("signedInUser");
+        const accountInfo = getAccountInfo(signedInUser);
+        updateProfileText(accountInfo);
+    } else {
+        signInButton.style.display = "inline-block";
+        profileButton.style.display = "none";
+
+        // Clear the current account info when signing out
+        updateProfileText(null);
     }
 }
 
@@ -143,8 +145,8 @@ function updateLeaderboard() {
             const checkmarkImg = document.createElement("img");
             checkmarkImg.src = "https://cdn.discordapp.com/attachments/1061160749524860949/1176632201761271848/Untitled4_20231121141547.png?ex=656f9321&is=655d1e21&hm=b8f037c74b23f954c529858cb775a6a5b93cbe6bc7625a1e9714aac98f5a3402&";
             checkmarkImg.alt = "Verified";
-            checkmarkImg.style.width = "15px";
-            checkmarkImg.style.height = "15px";
+            checkmarkImg.style.width = "20px";
+            checkmarkImg.style.height = "20px";
             checkmarkImg.style.marginLeft = "5px"; // Adjust margin for spacing
             listItem.appendChild(checkmarkImg);
         }
