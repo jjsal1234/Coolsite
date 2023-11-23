013f8d71e45fb8896c45c6b9a010103a5245a91a
jjsal1234 <143920177+jjsal1234@users.noreply.github.com>
2023-11-23 14:32:59 -0700

    Update accounts.js

--- accounts.js
+++ accounts.js
@@ -2,7 +2,7 @@
 
 // Sample accounts data with the "Locked" attribute
 const accounts = [
-    { username: "Tester", verified: true, banned: false, locked: true },
+    { username: "[Deleted]", verified: true, banned: false, locked: true },
     { username: "jjsal1234", password: "jacobsux1omg", verified: true, banned: false, locked: false },
     { username: "Skeleton", password: "DfcKyt", verified: true, banned: false, locked: false },
     { username: "Cow", password: "HsdjaHjdJ", verified: false, banned: false, locked: false },
