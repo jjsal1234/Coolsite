3cc6b22094f1af9171730129ae60e4d694ca6295
jjsal1234 <143920177+jjsal1234@users.noreply.github.com>
2023-12-11 21:43:39 -0700

    Update debug.js

--- debug.js
+++ debug.js
@@ -1,74 +1,83 @@
-// debug.js
+// debugpass.js
 
 // Wait for the DOM to be fully loaded
 document.addEventListener("DOMContentLoaded", function () {
-    // Create a simulated admin panel
-    var adminPanel = document.createElement('div');
-    adminPanel.style.position = 'fixed';
-    adminPanel.style.top = '50%';
-    adminPanel.style.left = '50%';
-    adminPanel.style.transform = 'translate(-50%, -50%)';
-    adminPanel.style.background = 'rgba(0, 0, 0, 0.8)';
-    adminPanel.style.color = 'white';
-    adminPanel.style.padding = '20px';
-    adminPanel.style.zIndex = '9999';
-    adminPanel.style.display = 'none'; // Initially hidden
+    // Create a password prompt
+    var passwordPrompt = document.createElement('div');
+    passwordPrompt.style.position = 'fixed';
+    passwordPrompt.style.top = '50%';
+    passwordPrompt.style.left = '50%';
+    passwordPrompt.style.transform = 'translate(-50%, -50%)';
+    passwordPrompt.style.background = 'rgba(0, 0, 0, 0.8)';
+    passwordPrompt.style.color = 'white';
+    passwordPrompt.style.padding = '20px';
+    passwordPrompt.style.zIndex = '9999';
 
-    // Password for the admin panel
-    var adminPassword = 'password123';
+    // Password for the password prompt
+    var password = 'Password123';
 
-    // Add some content to the admin panel
-    adminPanel.innerHTML = `
-        <h2>Admin Panel</h2>
-        <label for="panelPassword">Password:</label>
-        <input type="password" id="panelPassword"><br><br>
-        <button onclick="openPanel()">Open Panel</button>
-        <button onclick="showPanel()">Show Panel</button><br><br>
-        <button onclick="simulateCriticalError()">Simulate Critical Error</button>
-        <button onclick="simulateWarning()">Simulate Warning</button>
-        <button onclick="simulateInformation()">Simulate Information</button>
+    // Add some content to the password prompt
+    passwordPrompt.innerHTML = `
+        <h2>Password Prompt</h2>
+        <label for="passwordInput">Enter Password:</label>
+        <input type="password" id="passwordInput">
+        <br><br>
+        <button onclick="checkPassword()">Submit</button>
     `;
 
-    // Append the admin panel to the body
-    document.body.appendChild(adminPanel);
+    // Append the password prompt to the body
+    document.body.appendChild(passwordPrompt);
 
-    // Function to display messages on the page
-    function displayDebugMessage(message) {
-        var debugMessage = document.getElementById('debugMessage');
-        if (debugMessage) {
-            debugMessage.innerText = message;
-        }
-    }
-
-    // Function to open the admin panel
-    window.openPanel = function () {
-        var passwordInput = document.getElementById('panelPassword');
-        if (passwordInput.value === adminPassword) {
-            adminPanel.style.display = 'block';
-            passwordInput.value = ''; // Clear the password field
+    // Function to check the entered password
+    window.checkPassword = function () {
+        var passwordInput = document.getElementById('passwordInput');
+        if (passwordInput.value === password) {
+            // Password is correct, close the password prompt
+            document.body.removeChild(passwordPrompt);
+            // Open the admin panel
+            showPanel();
         } else {
             alert('Incorrect password!');
         }
     };
 
     // Function to show the admin panel without a password
-    window.showPanel = function () {
-        adminPanel.style.display = 'block';
-        // Optionally, you can hide other elements here if needed
-    };
+    function showPanel() {
+        // Create a simulated admin panel
+        var adminPanel = document.createElement('div');
+        adminPanel.style.position = 'fixed';
+        adminPanel.style.top = '50%';
+        adminPanel.style.left = '50%';
+        adminPanel.style.transform = 'translate(-50%, -50%)';
+        adminPanel.style.background = 'rgba(0, 0, 0, 0.8)';
+        adminPanel.style.color = 'white';
+        adminPanel.style.padding = '20px';
+        adminPanel.style.zIndex = '9999';
+
+        // Add some content to the admin panel
+        adminPanel.innerHTML = `
+            <h2>Admin Panel</h2>
+            <button onclick="simulateCriticalError()">Simulate Critical Error</button>
+            <button onclick="simulateWarning()">Simulate Warning</button>
+            <button onclick="simulateInformation()">Simulate Information</button>
+        `;
+
+        // Append the admin panel to the body
+        document.body.appendChild(adminPanel);
+    }
 
     // Simulate critical error
     function simulateCriticalError() {
-        displayDebugMessage("Debug Mode: Critical Error!");
+        alert("Debug Mode: Critical Error!");
     }
 
     // Simulate warning
     function simulateWarning() {
-        displayDebugMessage("Debug Mode: Warning - This is just a simulation.");
+        alert("Debug Mode: Warning - This is just a simulation.");
     }
 
     // Simulate information
     function simulateInformation() {
-        displayDebugMessage("Debug Mode: Information - This is for testing purposes only.");
+        alert("Debug Mode: Information - This is for testing purposes only.");
     }
 });
