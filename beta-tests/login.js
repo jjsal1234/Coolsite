50ee28f72e17c4b99e6fe06c1b5ecf8a49590361
jjsal1234 <143920177+jjsal1234@users.noreply.github.com>
2023-12-14 23:35:31 -0700

    Update login.js

--- beta-tests/login.js
+++ beta-tests/login.js
@@ -4,7 +4,7 @@ function attemptLogin() {
     const username = document.getElementById("username").value;
     const password = document.getElementById("password").value;
 
-    // Perform authentication logic (read from a text file hosted on GitHub)
+    // Perform authentication logic (read from the hosted text file)
     authenticateUser(username, password);
 }
 
@@ -21,11 +21,14 @@ function authenticateUser(username, password) {
 
             // Check if the provided username exists and the password matches
             if (users.hasOwnProperty(username) && users[username] === password) {
-                // Set a cookie to indicate the user is logged in
-                document.cookie = "loggedIn=true; path=/";
+                if (username === 'user1' && password === '1234') {
+                    document.getElementById("loginStatus").innerText = "Admin login successful!";
+                } else {
+                    document.getElementById("loginStatus").innerText = "Login successful!";
+                }
 
-                // Redirect to home.html
-                window.location.href = "home.html";
+                // Store user information in localStorage
+                localStorage.setItem('loggedInUser', JSON.stringify({ username, password }));
             } else {
                 document.getElementById("loginStatus").innerText = "Invalid credentials. Please try again.";
             }
@@ -35,10 +38,12 @@ function authenticateUser(username, password) {
         });
 }
 
-// Check for the loggedIn cookie on page load
+// Check for stored login information on page load
 window.onload = function () {
-    if (document.cookie.includes("loggedIn=true")) {
-        // If the user is already logged in, redirect to home.html
-        window.location.href = "home.html";
+    const storedUser = localStorage.getItem('loggedInUser');
+
+    if (storedUser) {
+        const { username, password } = JSON.parse(storedUser);
+        authenticateUser(username, password);
     }
 };
