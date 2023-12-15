81b7d8dc1c4bc471215c6b9bed9e48891e5417ee
jjsal1234 <143920177+jjsal1234@users.noreply.github.com>
2023-12-14 23:31:15 -0700

    Update login.js

--- beta-tests/login.js
+++ beta-tests/login.js
@@ -4,13 +4,13 @@ function attemptLogin() {
     const username = document.getElementById("username").value;
     const password = document.getElementById("password").value;
 
-    // Perform authentication logic (read from the provided users.txt file)
+    // Perform authentication logic (read from a text file hosted on GitHub)
     authenticateUser(username, password);
 }
 
 function authenticateUser(username, password) {
     // Link to the users.txt file hosted on GitHub Pages
-    const usersFileURL = 'https://jjsal1234.github.io/Coolsite/accounts/users.txt';
+    const usersFileURL = 'https://jjsal1234.github.io/data/random/GSug5.txt';
 
     // Read user credentials from the text file
     fetch(usersFileURL)
@@ -21,16 +21,24 @@ function authenticateUser(username, password) {
 
             // Check if the provided username exists and the password matches
             if (users.hasOwnProperty(username) && users[username] === password) {
-                if (username === 'user1' && password === '1234') {
-                    document.getElementById("loginStatus").innerText = "Admin login successful!";
-                } else {
-                    document.getElementById("loginStatus").innerText = "Login successful!";
-                }
+                // Set a cookie to indicate the user is logged in
+                document.cookie = "loggedIn=true; path=/";
+
+                // Redirect to home.html
+                window.location.href = "home.html";
             } else {
                 document.getElementById("loginStatus").innerText = "Invalid credentials. Please try again.";
             }
         })
         .catch(error => {
-            console.error('Error reading users.txt:', error);
+            console.error('Error reading GSug5.txt:', error);
         });
 }
+
+// Check for the loggedIn cookie on page load
+window.onload = function () {
+    if (document.cookie.includes("loggedIn=true")) {
+        // If the user is already logged in, redirect to home.html
+        window.location.href = "home.html";
+    }
+};
