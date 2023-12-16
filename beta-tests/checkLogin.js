8f50892c8c6cf3a3a11639f2bf751920142ceec9
jjsal1234 <143920177+jjsal1234@users.noreply.github.com>
2023-12-15 22:34:41 -0700

    Delete beta-tests/checkLogin.js

--- beta-tests/checkLogin.js
+++
@@ -1,9 +0,0 @@
-// Check for stored login information on page load
-window.onload = function () {
-    const storedUser = localStorage.getItem('loggedInUser');
-
-    if (!storedUser) {
-        // Redirect to the login page if not logged in
-        window.location.href = 'login.html';
-    }
-};
